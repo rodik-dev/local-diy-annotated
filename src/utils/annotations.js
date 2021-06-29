85804710644d36600ee0a08e405904986fdb37f7
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 22:00:50 +0300

    annotations.js: changed

--- src/utils/annotations.js
+++ src/utils/annotations.js
@@ -9,6 +9,8 @@ export default function makeAnnotationAttribute(arr) {
         }
 
         acc += fieldPath;
-        if (location)        
+        if (location) {
+            acc += 
+        }
     }).join(' ');
 }
