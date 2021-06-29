c8604acc5132669599904d37c2d589ab82f5adc4
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 21:58:03 +0300

    annotations.js: changed

--- src/utils/annotations.js
+++ src/utils/annotations.js
@@ -1,6 +1,6 @@
 export default function makeAnnotation(arr) {
     arr.reduce((acc, [objectId, fieldPath, location]) => {
-        return acc += ` ${objectId ? objectId}`
+        return acc += ` ${objectId}`
     }, '')
     return condition ? { [name]: value } : null;
 }
