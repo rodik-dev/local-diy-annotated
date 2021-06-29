b4fa59ccbc9fb4ff53d2d3944f8e72a7281160ec
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 22:04:55 +0300

    annotations.js: changed

--- src/utils/annotations.js
+++ src/utils/annotations.js
@@ -2,7 +2,7 @@
  * receive an array of arrays that represent annotations, and format into annotation attribute
  * @param {[[string]]]} arr array of arrays of annotaion specifiers
  */
-export default function makeAnnotation(arr) {
+export function makeAnnotation(arr) {
     return arr.map(([objectId, fieldPath, location]) => {
         let str = '';
         if (objectId) {
