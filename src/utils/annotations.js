118771c1ccd041c627509bfafea7d9a792091edb
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 21:58:47 +0300

    annotations.js: changed

--- src/utils/annotations.js
+++ src/utils/annotations.js
@@ -1,5 +1,5 @@
 export default function makeAnnotation(arr) {
-    arr.reduce((acc, [objectId, fieldPath, location]) => {
+    arr.map((acc, [objectId, fieldPath, location]) => {
         
         if (objectId) {
             acc += ''
