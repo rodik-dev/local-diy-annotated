cb901be77e44448418505f49ab2630f34a3c1a7b
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 21:55:42 +0300

    annotations.js: changed

--- src/utils/annotations.js
+++ src/utils/annotations.js
@@ -1,6 +1,4 @@
 export default function makeAnnotation(arr) {
-    if (typeof condition === 'undefined') {
-        condition = true;
-    }
+    
     return condition ? { [name]: value } : null;
 }
