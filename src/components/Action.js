7a09fe26f0569cc3a86dce0c232a3127480ce87b
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 22:11:51 +0300

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -35,7 +35,6 @@ export default class Action extends React.Component {
         const annotations = [
             '.url/@href',
             `.label${showIcon ? '/span[1]' : ''}`,
-
         ];
         if (showIcon) { 
             annotations.push('.icon/svg[1]');
